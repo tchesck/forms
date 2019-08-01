70e55221012e7044e879cc48d1999b11a138bf43
matheus s. antonio <viciogamer157@gmail.com>
2019-08-01 14:14:25 -0300

    fazer forms

---
+++ script.js
@@ -0,0 +1,39 @@
+let nome = document.querySelector(`#nome`);
+let email = document.querySelector(`#email`);
+let confirme = document.querySelector(`#confirme`);
+let senha = document.querySelector(`#senha`);
+let sexo =document.querySelector(`sexo`)
+let button = document.querySelector(`buttan`)
+if(email == confirme){
+    console.log(`${email}, confirmado`)
+    
+
+
+}
+
+function aceitar(){
+    localStorage.setItem()
+
+    
+}
+botao.onclick 
+else if(){
+
+}
+
+
+else if(){
+
+}
+
+
+else if(){
+     
+}
+
+
+else if(){}
+
+
+
+else()
